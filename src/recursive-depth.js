0b4ce710509307ba1da6426e6992543521edfbc9
Angelina <zhuravskayalina@gmail.com>
2022-10-14 21:41:51 +0400

    feat: implement recursive-depth function

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,11 +1,11 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {NotImplementedError} = require('../extensions/index.js');
 
 /**
  * Implement class DepthCalculator with method calculateDepth
  * that calculates deoth of nested array
- * 
+ *
  * @example
- * 
+ *
  * const depthCalc = new DepthCalculator();
  * depthCalc.calculateDepth([1, 2, 3, 4, 5]) => 1
  * depthCalc.calculateDepth([1, 2, 3, [4, 5]]) => 2
@@ -13,12 +13,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    calculateDepth(arr) {
+        let depth = 1;
+
+        arr.forEach(el => {
+            if (Array.isArray(el)) {
+                depth = Math.max(depth, this.calculateDepth(el) + 1) // + 1 bc if we are there its +1 array
+            }
+        })
+        return depth;
+    }
 }
 
+
 module.exports = {
-  DepthCalculator
+    DepthCalculator
 };
--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,32 +1,55 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {NotImplementedError} = require('../extensions/index.js');
+const {assert} = require("chai");
 
 /**
  * Implement chainMaker object according to task description
- * 
+ *
  */
+
 const chainMaker = {
-  getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    chain: [],
+
+    getLength() {
+        return this.chain.length;
+    },
+
+    addLink(value = '') {
+        const chainEl = `( ${value} )~~`;
+        this.chain.push(chainEl);
+        return this;
+    },
+    removeLink(position) {
+        if (typeof position !== 'number' || position < 1 || position > this.chain.length || !Number.isInteger(position)) {
+            this.chain.length = 0;
+            throw new Error(`You can\'t remove incorrect link!`)
+        }
+        this.chain.splice(position - 1, 1);
+        return this;
+    },
+
+    reverseChain() {
+        if (this.chain.length !== 1) {
+            this.chain.reverse();
+        }
+        return this;
+    },
+
+    finishChain() {
+        const lastEl = this.chain[this.chain.length - 1].split('~~').join('');
+        this.chain.pop();
+        this.chain.push(lastEl);
+        let result = this.chain.join('');
+        this.chain.length = 0;
+        return result;
+    }
 };
 
+console.log(chainMaker.addLink(function () { })
+    .addLink('2nd').addLink('3rd')
+    .removeLink(2).reverseChain()
+    .finishChain()) // '( 3rd )~~( function () { } )');
+
+
 module.exports = {
-  chainMaker
+    chainMaker
 };
